d97e3dc7db3a261b570d175e9571612a94f20158
Muhammad Fahmi Alamsyah <13519077@std.stei.itb.ac.id>
2022-04-01 06:45:12 +0700

    fixing navbar

--- src/component/Head/Header.js
+++ src/component/Head/Header.js
@@ -13,41 +13,31 @@ const Header = () => {
   return (
     <>
       <header className='header'>
-        <div className='container d_flex'>
-          <div className='logo'>
-            <h1>Alamsyah</h1>
-          </div>
+        <div className='logo'>
+          <h1>Alamsyah</h1>
+        </div>
 
-          <div className='navlink'>
-            <ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onClick={() => setMobile(false)}>
-              {/*<ul className='link f_flex uppercase {Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}'>*/}
-              <li>
-                <a href='#home'>Home</a>
-              </li>
-              <li>
-                <a href='#features'>Skills</a>
-              </li>
-              <li>
-                <a href='#portfolio'>Portfolio</a>
-              </li>
-              <li>
-                <a href='#resume'>Resume</a>
-              </li>
-              {/* <li>
-                <a href='#clients'>Testimonials</a>
-              </li> */}
-              <li>
-                <a href='#blog'>Certification</a>
-              </li>
-              <li>
-                <a href='#contact'>Contact</a>
-              </li>
-            </ul>
-
-            <button className='toggle' onClick={() => setMobile(!Mobile)}>
-              {Mobile ? <i className='fas fa-times close home-btn'></i> : <i className='fas fa-bars open'></i>}
-            </button>
-          </div>
+        <div className='navlink'>
+          <ul className="ul-link">
+            <li>
+              <a href='#home'>Home</a>
+            </li>
+            <li>
+              <a href='#features'>Skills</a>
+            </li>
+            <li>
+              <a href='#portfolio'>Portfolio</a>
+            </li>
+            <li>
+              <a href='#resume'>Resume</a>
+            </li>
+            <li>
+              <a href='#blog'>Certification</a>
+            </li>
+            <li>
+              <a href='#contact'>Contact</a>
+            </li>
+          </ul>
         </div>
       </header>
     </>
